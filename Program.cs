b593ec919c378a0aadd65bfebd37fb29a611924c
Patrik Endler <U02S1J6@post.at>
2021-09-10 10:08:05 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        private static int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        private static int target = 32;
+        private static string romanNumber = "MCMXCIV";
+
+        static void Main(string[] args)
+        {
+            TwoSum.Program.getNumberIndexWithTwoPointer(numberArray, target);
+        }
+    }
+}
